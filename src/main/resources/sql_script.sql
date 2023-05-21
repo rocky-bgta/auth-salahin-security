b3172732777c471b8e14c71f8ed266dd2a0344c8
Salahin <rocky.bgta@gmail.com>
2023-05-21 18:38:57 +0600

    add oauth token script

--- src/main/resources/sql_script.sql
+++ src/main/resources/sql_script.sql
@@ -9,4 +9,21 @@ INSERT INTO user_role(user_id, role_id) VALUES(2,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,2);
 
 
+CREATE TABLE oauth_access_token (
+                                    authentication_id varchar(255) NOT NULL PRIMARY KEY,
+                                    token_id varchar(255) NOT NULL,
+                                    token blob NOT NULL,
+                                    user_name varchar(255) NOT NULL,
+                                    client_id varchar(255) NOT NULL,
+                                    authentication blob NOT NULL,
+                                    refresh_token varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE oauth_refresh_token (
+                                     token_id varchar(255) NOT NULL,
+                                     token blob NOT NULL,
+                                     authentication blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
